2afc5ec75aec9d6d5d8f911f97cad351b9e823a2
Ramakant Debata <ramakant.s.debata@gmail.com>
2025-05-19 17:31:35 +0530

    Day 1

--- _03_Add.py
+++ _03_Add.py
@@ -1,7 +1,11 @@
-def add(a, b):
+def add(a:int, b:int) -> int:
    res = a + b
    if a == 0:
         print("A is '0'")
    return res
 
 print(add(1, 2))
+
+print(add(1.5, 2.5))
+
+print(add("Test", "Data"))
